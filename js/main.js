9893f5bfd89332b77292d0553d9a33cc4194a1af
Soraya Ribeiro <soraya.alr@gmail.com>
2022-02-13 18:37:08 -0300

    Projeto João Lemon

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Declarando variaveis
+var btnContact = document.querySelector('.jl-btn-contact');
+
+//Page preloader
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+ 
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+});
+
+
+//Abrindo e fechando informações de contato
+btnContact.addEventListener('click', function(){
+    var boxContact = document.querySelector('.jl-contact-info');
+    boxContact.classList.toggle('jl-is-open');
+    this.classList.toggle('jl-change-icon');
+
+});
